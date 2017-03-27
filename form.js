959da05550d54410efa9d457d7bca8777afb9d7a
Carmen Buezo <carmen.buezo@yahoo.com>
2017-03-27 11:24:49 -0400

    "fixed with array"

--- form.js
+++ form.js
@@ -1,42 +1,41 @@
-$(document).ready(function()
-{
-	/*
-	** put all your code inside of the $(document).ready function to make sure the html and CSS has been loaded 
-	** properly before running the JS code
-	*/
+$(document).ready(function() {
+    /*
+     ** put all your code inside of the $(document).ready function to make sure the html and CSS has been loaded 
+     ** properly before running the JS code
+     */
 
-	
-	/*
-	** this is the callback function that is invoked when the user clicks on the submit button
-	*/
-	$('#submitButton').click(function (e) {
 
-		// call prevent defualt here to disable the 
-		// default action of clearing the input values
-		e.preventDefault();
+    /*
+     ** this is the callback function that is invoked when the user clicks on the submit button
+     */
+    var greetings = ["Hello", "Hola", "Bonjour"];
 
-		// get the value entered by the user into the text input box
-		var name = $("#textInput").val();
+    $('#submitButton').click(function(e) {
 
-		if (name != "")
-		{
-			// if the user entered a name, then set a greeeting in the feedback text box
-			$('#greetingText').text ("Hello " + name);
-		}
-		
-	});
-	
+        // call prevent defualt here to disable the 
+        // default action of clearing the input values
+        e.preventDefault();
+        //get selected radio button val and change it to a number
+       var radioValue = $("input[name = 'language']:checked").val();
+        radioValue = Number(radioValue);
+        // get the value entered by the user into the text input box
+        var name = $("#textInput").val();
 
-	/*
-	** this is the callback function that is invoked when the user clicks on the clear button
-	*/
-	$('#clearButton').click(function (e) {
+        if (name != "") {
+            // if the user entered a name, then set a greeeting in the feedback text box
+            $('#greetingText').text(greetings[radioValue] + ' ' + name);
+        };
+    });
+   
+    // this is the callback function that is invoked when the user clicks on the clear button
+ 
+    $('#clearButton').click(function(e) {
 
-		// set the feedback text area to blank to clear it; note that we dont call preventDefault here, because we do 
-		// want the default action to clear the name text input area which is the default action
-		$('#greetingText').text ("");		
+        // set the feedback text area to blank to clear it; note that we dont call preventDefault here, because we do 
+        // want the default action to clear the name text input area which is the default action
+        $('#greetingText').text("");
 
-	});
+    });
 
 
 });
